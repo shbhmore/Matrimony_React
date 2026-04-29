d3535bab79432bc4779728189d9c9c6d3af8ef3b
shbhmore <mr.smore77@gmail.com>
2026-04-30 00:37:21 +0530

    Silly mistakes, but now finally done

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import NotFound from './pages/NotFound';
 import HomePage from './pages/HomePage';
 import { Navigate, Route, Router, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import BiodataModal from './pages/BiodataModal';
 
 function App() {
   const ProtectedRoute = ({ children }) => {
